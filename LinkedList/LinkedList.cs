08ad36884d514b1683d3448a5a0f900fd71052ce
Xinyi <xinzwerkz@gmail.com>
2021-08-31 14:48:26 +0800

    Add GetAt, AddString, Insert, RemoveAt, Count methods

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -12,6 +12,85 @@ namespace LinkedList
             numElem = 0;
         }
 
-        public 
+        private Node GetAt(int index)
+        {
+            Node currNode = Head;
+
+            for (int i = 0; i < index; i++)
+            {
+                currNode = currNode.Next;
+            }
+
+            return currNode;
+        }
+
+        public void AddString(string newElement)
+        {
+            Node newNode = new Node(newElement);
+
+            if (numElem == 0)
+            {
+                Head = newNode;
+                return;
+            }
+
+            Node lastNode = GetAt(numElem - 1);
+            lastNode.Next = newNode;
+            numElem++;
+        }
+
+        public void Insert(int index, string newElement)
+        {
+            if (index >= 0 && index <= numElem)
+            {
+                Node newNode = new Node(newElement);
+
+                if (index == 0)
+                {
+                    newNode.Next = Head;
+                    Head = newNode;
+                }
+
+                Node nodeBefore = GetAt(index - 1);
+                Node nodeAfter = nodeBefore.Next;
+
+                nodeBefore.Next = newNode;
+                newNode.Next = nodeAfter;
+            }
+
+            numElem++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= numElem)
+            {
+                return;
+            }
+
+            if (index == 0)
+            {
+                Head = Head.Next;
+                return;
+            }
+
+            Node nodeBefore = GetAt(index - 1);
+            Node nodeToRemove = GetAt(index);
+
+            nodeBefore.Next = nodeToRemove.Next;
+
+            numElem--;
+        }
+
+        public int Count()
+        {
+            return numElem;
+        }
+
+        public void Replace(int index, string newElement)
+        {
+            if (index < 0 || index > numElem;)
+        }
+        
     }
 }
